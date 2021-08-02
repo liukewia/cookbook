012711e96279c8e52e7348244d0d327e9ee248a5
Yi Liu <2587395l@student.gla.ac.uk>
2021-08-02 23:53:45 +0800

    update

--- rango/urls.py
+++ rango/urls.py
@@ -9,12 +9,12 @@ urlpatterns = [
     path('', views.index),
     # match all other pages
     # https://stackoverflow.com/questions/40826295/react-routing-and-django-url-conflict
+    path('category/<slug:category_name_slug>/',
+         views.show_category, name='show_category'),
     re_path(r'^(?:.*)/?$', views.index),
     # path('user/login/', views.user_login),
 
     # path('about/', views.about, name="about"),
-    path('category/<slug:category_name_slug>/',
-         views.show_category, name='show_category'),
     # path('add_category/', views.add_category, name='add_category'),
     # path('category/<slug:category_name_slug>/add_page/',
     #      views.add_page, name='add_page'),
