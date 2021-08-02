ccc536fe7b0bde9440f713df3e92c035627f3002
SH-giant <1481874180@qq.com>
2021-08-02 23:36:03 +0800

    rebuild category

--- rango/views.py
+++ rango/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 
 from rango.forms import CategoryForm, PageForm
-from rango.models import Category, Page
+from rango.models import Category, Page, Recipe
 
 
 def index(request):
@@ -14,6 +14,31 @@ def index(request):
 
 def user_login(request):
     return render(request, 'user/login/index.html')
+
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
+
+
+def show_recipe(request, recipe_title):
+    context_dict = {}
+
+    recipe = Recipe.objects.get(title=recipe_title)
+    context_dict['recipe'] = recipe
+
+    render(request, '', context=context_dict)
+
 
 
 # def index(request):
@@ -40,19 +65,19 @@ def about(request):
     return render(request, 'rango/about.html', context=context_dict)
 
 
-def show_category(request, category_name_slug):
-    context_dict = {}
-
-    try:
-        category = Category.objects.get(slug=category_name_slug)
-        pages = Page.objects.filter(category=category)
-        context_dict['pages'] = pages
-        context_dict['category'] = category
-    except Category.DoesNotExist:
-        context_dict['category'] = None
-        context_dict['pages'] = None
-
-    return render(request, 'rango/category.html', context=context_dict)
+# def show_category(request, category_name_slug):
+#     context_dict = {}
+#
+#     try:
+#         category = Category.objects.get(slug=category_name_slug)
+#         pages = Page.objects.filter(category=category)
+#         context_dict['pages'] = pages
+#         context_dict['category'] = category
+#     except Category.DoesNotExist:
+#         context_dict['category'] = None
+#         context_dict['pages'] = None
+#
+#     return render(request, 'rango/category.html', context=context_dict)
 
 
 @login_required
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,16 +15,16 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 
 from rango import views
 
 urlpatterns = [
                   # match the root
                   path('', views.index),
+                  path('api/', include('rango.urls')),
                   # match all other pages
                   re_path(r'^(?:.*)/?$', views.index),
-                  # path('', include('rango.urls')),
                   # path('user/login/', views.user_login),
                   # path('', views.index, name='index'),
                   # path('rango/', include('rango.urls')),
