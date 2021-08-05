27b5d2225af5ecc0b41267a6cb02b83027e6c851
Yi Liu <2587395l@student.gla.ac.uk>
2021-08-05 21:48:59 +0800

    update

--- cookbook-web/dist/p__CategoryHub.64594c65.async.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkcookbook_web=self.webpackChunkcookbook_web||[]).push([[256],{79395:function(E,o,n){"use strict";n.r(o),n.d(o,{default:function(){return e}});var a=n(70844),t=n(19630),u=n(85893);function e(){var c=(0,t.TH)(),r=c.pathname,s=r.replace(/\//g,"");return(0,u.jsx)(a.ZP,{children:s})}}}]);
